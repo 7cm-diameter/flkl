f5aa8a5dfc0a206ad316d4cfef3ea0b82224c0de
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2024-01-09 18:46:05 +0900

    Add sound trial

--- flkl/pavlovian.py
+++ flkl/pavlovian.py
@@ -1,7 +1,8 @@
 from amas.agent import Agent, NotWorkingError
 import numpy as np
+from numpy.random import uniform
 from utex.agent import AgentAddress
-from utex.shceduler import SessionMarker, TrialIterator, blockwise_shuffle, mix, repeat
+from utex.scheduler import SessionMarker, TrialIterator, blockwise_shuffle2, mix, repeat
 
 from flkl.share import Flkl
 
@@ -21,6 +22,14 @@ async def conditional_discrimination(agent: Agent, ino: Flkl, expvars: dict):
     go_signals = list(filter(lambda hz: hz > boundary, led_flick_hz))
     nogo_signals = list(filter(lambda hz: hz < boundary, led_flick_hz))
     go_nogo_signals = mix(go_signals, nogo_signals, go_ratio, nogo_ratio)
+    visual_trial = repeat(1, len(go_nogo_signals))
+    sound_trial = repeat(0, len(sound_flick_hz))
+    visual_ratio = expvars.get("visual-ratio", 1)
+    sound_ratio = expvars.get("audio-ratio", 1)
+    go_nogo_signals_with_sound = mix(
+        go_nogo_signals, sound_flick_hz, visual_ratio, sound_ratio
+    )
+    av_trials = mix(visual_trial, sound_trial, visual_ratio, sound_ratio)
 
     intertrial_interval = expvars.get("ITI", 3.0)
     iti_range = expvars.get("ITI-range", 1.0)
@@ -30,22 +39,38 @@ async def conditional_discrimination(agent: Agent, ino: Flkl, expvars: dict):
         intertrial_interval + iti_range,
         number_of_trial,
     )
-    flick_each_trial = blockwise_shuffle(
-        repeat(go_nogo_signals, number_of_trial // len(go_nogo_signals) + 1),
-        len(go_nogo_signals),
+    flick_each_trial = blockwise_shuffle2(
+        repeat(
+            go_nogo_signals_with_sound,
+            number_of_trial // len(go_nogo_signals_with_sound) + 1,
+        ),
+        repeat(
+            av_trials,
+            number_of_trial // len(av_trials) + 1,
+        ),
+        len(go_nogo_signals_with_sound),
     )
-    trials = TrialIterator(flick_each_trial[0:number_of_trial], itis)
+    trials = TrialIterator(
+        av_trials[:number_of_trial], flick_each_trial[:number_of_trial], itis
+    )
 
     try:
         while agent.working():
-            for i, flick, iti in trials:
+            for i, is_visual, flick, iti in trials:
                 print(f"{i} trial: flick with {flick} hz after {iti} sec")
                 await agent.sleep(iti)
-                ino.flick_for(led_pin, flick, flick_duration)
-                await agent.sleep(flick_duration / 1000)
-                if flick > boundary:
-                    ino.high_for(reward_pin, reward_duration)
-                    await agent.sleep(reward_duration / 1000)
+                if is_visual:
+                    ino.flick_for(led_pin, flick, flick_duration)
+                    await agent.sleep(flick_duration / 1000)
+                    if flick > boundary:
+                        ino.high_for(reward_pin, reward_duration)
+                        await agent.sleep(reward_duration / 1000)
+                else:
+                    ino.flick_for(led_pin, flick, flick_duration)
+                    await agent.sleep(flick_duration / 1000)
+                    if uniform() <= 0.5:
+                        ino.high_for(reward_pin, reward_duration)
+                        await agent.sleep(reward_duration / 1000)
             agent.send_to(AgentAddress.OBSERVER.value, SessionMarker.NEND)
     except NotWorkingError:
         pass
@@ -62,7 +87,7 @@ if __name__ == "__main__":
     )
     from utex.clap import PinoClap
     from utex.agent import self_terminate, Reader, Observer, Recorder
-    from utex.shceduler import SessionMarker
+    from utex.scheduler import SessionMarker
     from amas.connection import Register
     from amas.env import Environment
 
